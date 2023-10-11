a28b6f06333202d1b6c4dc787b57de6492c27083
Vishnuvardhan Reddy <tvishnuvardhanreddy24@gmail.com>
2023-10-11 16:25:49 +0530

    calculator code

---
+++ HTML Codes/Calculator/index.js
@@ -0,0 +1,26 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    
+    if (value === 'AC') {
+        displayValue = '';
+    } else {
+        displayValue += value;
+    }
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+    
